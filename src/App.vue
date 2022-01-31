6b0191a59cd7e6eb1adec93332fd53eca2a8de17
amer1can <amer1can@mail.ru>
2022-01-31 17:10:21 +0300

    initial comment: realized connection to metamask wallet and make wave button to write and read from rinkeby blockchain

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,103 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png"><br>
+  <button @click="connectToWallet">Connect</button><br>
+  <button @click="wave">Make a wave</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      contractAddress: "0x39b351095a6e8664a53d38bc0aC231Fb9a21A966",
+      contractABI: abi.abi,
+    }
+  },
+  mounted() {
+    this.checkIfWalletIsConnected();
+  },
+  methods: {
+    async checkIfWalletIsConnected() {
+      try {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+        } else {
+          console.log("We have ethereum object", ethereum);
+        }
+
+        const accounts = await ethereum.request({
+          method: "eth_accounts"
+        });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          //setCurrentAccount(account)
+        } else {
+          console.log("No authorized account found");
+        }
+
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async connectToWallet() {
+      const { ethereum } = window;
+
+      try {
+        if (!ethereum) {
+          console.log("Get Metamask!");
+          return;
+        }
+
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts"
+        });
+        console.log("Connected", accounts[0])
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async wave() {
+      try {
+        const { ethereum } = window;
+
+        if(ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const wavePortalContract = new ethers.Contract(
+              this.contractAddress,
+              this.contractABI,
+              signer
+          );
+
+          let count = await wavePortalContract.getTotalWaves();
+          console.log("Retrieved total wave count...", count.toNumber())
+
+          const waveTxn = await wavePortalContract.wave();
+          console.log("Mining...", waveTxn.hash);
+
+          await waveTxn.wait();
+          console.log("Mined -- ", waveTxn.hash);
+
+          count = await wavePortalContract.getTotalWaves();
+          console.log("Retrieved total wave count...", count.toNumber())
+
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+
+      } catch (err) {
+        console.log(err)
+      }
+    }
   }
 }
 </script>
